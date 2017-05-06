2662b4d299ffe767eb19562ca39e66920e9d9661
Subigya Jyoti Panta <subigyapanta@gmail.com>
2017-05-06 19:47:39 +0700

    Some variables made private and authenticated returns boolean value

--- src/UserAuthentication.py
+++ src/UserAuthentication.py
@@ -1,38 +1,42 @@
 import xmlrpclib
 
 class UserAuthentication:
-    authenticated = False
+    __authenticated = False
 
-    url = 'http://localhost:8069'
-    db = 'odoo'
-    username = 'api@subigya.com'
-    password = 'api@subigya.com'
+    __url = 'http://localhost:8069'
+    __db = 'odoo'
+    __username = 'api@subigya.com'
+    __password = 'api@subigya.com'
 
     def __init__(self, name, identification):
         self.name = name
         self.identification = identification
 
     def setUrl(self, url):
-        self.url = url
+        self.__url = url
 
     def setUrl(self, db):
-        self.db = db
+        self.__db = db
 
     def setUsername(self, username):
-        self.username = username
+        self.__username = username
 
     def setPassword(self, password):
-        self.password = password
+        self.__password = password
 
     def connect(self):
-        common = xmlrpclib.ServerProxy('{}/xmlrpc/2/common'.format(self.url))
-        self.uid = common.authenticate(self.db, self.username, self.password, {})
+        common = xmlrpclib.ServerProxy('{}/xmlrpc/2/common'.format(self.__url))
+        self.uid = common.authenticate(self.__db, self.__username, self.__password, {})
 
     def authenticateUser(self):
         self.connect()
-        models = xmlrpclib.ServerProxy('{}/xmlrpc/2/object'.format(self.url))
-        count = models.execute_kw(self.db, self.uid, self.password,
+        models = xmlrpclib.ServerProxy('{}/xmlrpc/2/object'.format(self.__url))
+        count = models.execute_kw(self.__db, self.uid, self.__password,
                           'hr.employee', 'search_count',
-                          [[['name_related', '=', self.name], ['identification_id', '=', self.identification]]])
+                                  [[['name_related', '=', self.name], ['identification_id', '=', self.identification]]])
+        if (count == 0):
+            self.__authenticated = False
+        elif(count == 1):
+            self.__authenticated = True
 
-        return count
+        return self.__authenticated
