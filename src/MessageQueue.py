f3c29add304316796c99f7f481c91c291423cad5
Nerisa <st118437@ait.asia>
2017-05-06 20:12:17 +0700

    Add code for getting message from queue

---
+++ src/MessageQueue.py
@@ -0,0 +1,40 @@
+import requests
+# import simplejson as json
+import json
+
+
+class MessageQueue():
+    REQUEST_QUEUE_URL = 'https://aitsgqueues.mrteera.com/messages/erp_request'
+    RESPONSE_QUEUE_URL = 'https://aitsgqueues.mrteera.com/messages/erp_response'
+
+    def erp_process_message_from_req_queue(self):
+        resp = requests.get(self.REQUEST_QUEUE_URL)
+        message_from_req = resp.json()
+
+        if ('{}' == str(message_from_req)):
+            print('No message in the queue: ' + str(message_from_req))
+            return None, None
+        else:
+            print('Found a message in the queue: ' + str(message_from_req))
+            message_id = message_from_req['id']
+            return message_from_req['message'], message_id
+
+    def erp_put_message_back_into_resp_queue(self, message, message_id):
+        if message == None:
+            return None
+        message = json.loads(message.encode('utf-8').replace("'", '"'))
+        resp_msg = "{\r\n\t\"gid\": \"%s\"" % message['username']
+        resp_msg += ",\r\n\t\"name\": \"noname\""
+        resp_msg += ",\r\n\t\"auth\": \"true\""
+        resp_msg += ",\r\n\t\"device_token\": \"%s\"\r\n}" % message['device-token']
+        payload = { "qname": "erp_response",
+            "message": resp_msg
+        }
+        resp = requests.post(self.RESPONSE_QUEUE_URL, payload)
+        print("erp_response status coode: " + str(resp.status_code))
+        if resp.status_code == 200:
+            resp = requests.delete('https://aitsgqueues.mrteera.com/messages/erp_request/{}'.format(message_id))
+            print('delete message status: ' + str(resp.status_code))
+
+test = MessageQueue()
+test.erp_process_message_from_req_queue();
