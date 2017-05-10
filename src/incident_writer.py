42907779f9851cd3ac833e015dc30123173c7151
Subigya Jyoti Panta <subigyapanta@gmail.com>
2017-05-11 00:55:32 +0700

    added try catch block to incident_writer

--- src/incident_writer.py
+++ src/incident_writer.py
@@ -1,24 +1,36 @@
 import logging
 from Incident import Incident
 from OdooConnector import OdooConnector
+from time import sleep
 import datetime
 import calendar
 
 if __name__ == '__main__':
-    logging.basicConfig(filename='../incident.log', level=logging.DEBUG, format="[%(filename)s:%(lineno)s - %(funcName)s ] %(message)s")
-    current_date = datetime.date.today()
-    _, num_days = calendar.monthrange(current_date.year, current_date.month)
-    start_date = datetime.date(current_date.year, current_date.month, 1)
-    end_date = datetime.date(current_date.year, current_date.month, num_days)
-    incident_status = Incident()
-    incident_counts = incident_status.get_incidents(start_date, end_date)
-    auth = OdooConnector()
-    for username in incident_counts.keys():
-        user_id = auth.find_user(username)
-        if len(user_id) is not 0:
-            auth.write_current_incident(user_id[0], incident_counts[username])
-        else:
-            logging.warn('No user with username[%s] in the system' % username)
+
+    logging.basicConfig(filename='../incident.log', level=logging.DEBUG,
+                    format="[%(filename)s:%(lineno)s - %(funcName)s ] %(message)s")
+    while True:
+
+        try:
+            current_date = datetime.date.today()
+            _, num_days = calendar.monthrange(current_date.year, current_date.month)
+            start_date = datetime.date(current_date.year, current_date.month, 1)
+            end_date = datetime.date(current_date.year, current_date.month, num_days)
+            incident_status = Incident()
+            incident_counts = incident_status.get_incidents(start_date, end_date)
+            auth = OdooConnector()
+            for username in incident_counts.keys():
+                user_id = auth.find_user(username)
+
+                if len(user_id) is not 0:
+                    auth.write_current_incident(user_id[0], incident_counts[username])
+                else:
+                    logging.warn('No user with username[%s] in the system' % username)
+
+        except Exception as e:
+            logging.error(e.message)
+
+        sleep(10)
 
 # auth = OdooConnector()
 # uid = auth.authenticate('somchai@mycompany.com', 'secret123')
